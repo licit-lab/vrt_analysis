ca25acfe8ff9c81251d958aa15b6d8a9eb484f71
Andres Ladino <aladinoster@gmail.com>
2020-10-09 18:53:37 +0200

    🚚  Moving files Carma + Analysis module

---
+++ collector/carma.py
@@ -0,0 +1,20 @@
+"""
+    This is a module to provide support classes for reading:
+    
+    Example:
+        To use the ``GetData`` declare in a string the ``path`` to the matfile ::
+        
+            >>> from collector.matlab import GetData
+            >>> x = GetData('data/raw/carma.data5.csv')
+
+"""
+
+# ============================================================================
+# STANDARD  IMPORTS
+# ============================================================================
+
+import pandas as pd
+
+# ============================================================================
+# CLASS AND DEFINITIONS
+# ============================================================================
